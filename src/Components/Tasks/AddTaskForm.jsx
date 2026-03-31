c230b60bba5510d60ecb407f4008419214a39e81
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-03-31 15:04:01 +0100

    feat: ajoute le change du valeur de text dans lors de l'exrit dans l'input d'ajoute de task

--- src/Components/Tasks/AddTaskForm.jsx
+++ src/Components/Tasks/AddTaskForm.jsx
@@ -1,8 +1,8 @@
 import AddTask from "../Button/AddTask";
 
-export default function AddTaskForm({addTask}) {
+export default function AddTaskForm({addTask , text , setText}) {
     return <form onSubmit={addTask}>
-        <input type="text" placeholder="add Task"/>
+        <input type="text" placeholder="add Task" value={text} onChange={(e) => {setText(e.target.value)}}/>
         <AddTask />
     </form>
 }
